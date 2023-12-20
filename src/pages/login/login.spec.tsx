16b81061af9df8bf6fa3640c58c4cf91563edbfd
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-12-20 19:14:44 +0530

    try this

--- src/pages/login/login.spec.tsx
+++ src/pages/login/login.spec.tsx
@@ -1,7 +1,5 @@
 import { test, expect } from "playwright-test-coverage";
 
-test.use({ viewport: { width: 500, height: 500 } });
-
 test("should work", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   const loginPage = await page.getByTestId("Login.Page");
