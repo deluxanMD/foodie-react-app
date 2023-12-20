901f504fdc1c1f7145df744f83419ecdb70084ce
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-12-20 19:25:45 +0530

    removing

--- src/pages/login/login.page.tsx
+++
@@ -1,5 +0,0 @@
-const Login = () => {
-  return <div data-testid="Login.Page">Login</div>;
-};
-
-export default Login;
--- tests/login.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "playwright-test-coverage";
-
-test("should work", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  const loginPage = await page.getByTestId("Login.Page");
-  expect(loginPage).toHaveText("Login");
-});
